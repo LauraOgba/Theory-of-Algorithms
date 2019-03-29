ef9a1810ed447d95b34e50d5d0c854e009b27b57
LauraOgba <lauraelohoogba4@gmail.com>
2019-03-29 11:16:06 +0000

    fixing errors

--- sha256.c
+++ sha256.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]){
 void sha256(){
 
  // Message schedule (Section 6.2).	
- uint32_t W[64};
+ uint32_t W[64];
  // Working Variables(section 6.2).
  uint32_t a, b, c, d, e, f, g, h;
  // Two temporary variables (section 6.2).
@@ -39,7 +39,7 @@ void sha256(){
 
 // The Hash Value
 // The values come from section 5.3.3
- uint35_t[8] = {
+ uint32_t[8] = {
 	 0x6a09e667
 	,0xbb67ae85
 	,0x3c6ef372
@@ -51,7 +51,7 @@ void sha256(){
  };
 
 // The current message block.
- uint32_t [16];
+ uint32_t M[16];
  
  // For looping.
  int t;
