0f023620061b2e52411a795f503eabe99623fc58
LauraOgba <lauraelohoogba4@gmail.com>
2019-03-29 21:34:45 +0000

    SHA256: Finishing out the padding

--- padfile.c
+++ padfile.c
@@ -5,8 +5,9 @@ union msgblock{
     uint8_t e[64];
     uint32_t t[16];
     uint64_t s[8];
-
 };
+
+enum status {READ, PAD0, PAD1, FINISH};
 
 int main(int argc, char *argv[]){
 
@@ -16,20 +17,33 @@ uint64_t nobits = 0;
 
 uint64_t nobytes;
 
+enum status S = READ;
+
 FILE* f;
 f = fopen(argv[1], "r");
 
-while (!feof(f)){
+while (S == READ){
     nobytes = fread(M.e, 1, 64, f);
+    printf("Read %211u bytes\n", nobytes);
     nobits = nobits + (nobytes * 8);
     if (nobytes < 56){
-printf("ive found a block that's less than 55 bytes!\n");
-M.e[nobytes] = 0x80;
-while (nobytes < 56){
+      printf("ive found a block that's less than 55 bytes!\n");
+        M.e[nobytes] = 0x80;
+        while (nobytes < 56){
     nobytes = nobytes + 1;
-    M.e[nobytes] = 0x00;
+    M.s[nobytes] = 0x00;
 }
 M.s[7] = nobits;
+S = FINISH;
+    }else if (nobytes < 64){
+        S = PAD0;
+        M.e[nobytes] = 0x80;
+        while (nobytes < 64){
+            nobytes = nobytes + 1;
+            M.e[nobytes] = 0x00;
+        }
+    }else if (feof(f)){
+        S = PAD1;
     }
 }
 
